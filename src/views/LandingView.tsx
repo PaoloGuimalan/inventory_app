f508201a4b072d9cd493dce3c1da1eb9c1758227
Paolo <pauloportes.guimalan187@gmail.com>
2025-04-27 15:17:20 +0800

    feat: product listing layout has been configured

--- src/views/LandingView.tsx
+++ src/views/LandingView.tsx
@@ -1,12 +1,17 @@
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 import React, {useCallback, useState} from 'react';
 import Layout from '../layouts/Layout';
 import HomeNavigations from '../widgets/HomeNavigations';
 import SearchHeader from '../widgets/SearchHeader';
-// import {useSelector} from 'react-redux';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import {RootStackParamList} from '../types/navigation';
+import {useNavigation} from '@react-navigation/native';
+
+type NavigationProp = NativeStackNavigationProp<RootStackParamList>;
 
 export default function LandingView() {
-  // const authentication = useSelector((state: any) => state.authentication);
+  const navigation = useNavigation<NavigationProp>();
 
   const [search, setsearch] = useState<string>('');
 
@@ -29,7 +34,26 @@ export default function LandingView() {
       }
       footer={<HomeNavigations />}>
       <View style={styles.container}>
-        <Text>LandingView</Text>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => {
+            navigation.navigate('Products');
+          }}>
+          <Icon name="category" size={80} color="#000" />
+          <Text style={styles.text}>Tools</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Icon name="category" size={80} color="#000" />
+          <Text style={styles.text}>Tools</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Icon name="category" size={80} color="#000" />
+          <Text style={styles.text}>Tools</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Icon name="category" size={80} color="#000" />
+          <Text style={styles.text}>Tools</Text>
+        </TouchableOpacity>
       </View>
     </Layout>
   );
@@ -39,5 +63,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#ffffff',
+    padding: 15,
+    justifyContent: 'center',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    gap: 10,
+  },
+  button: {
+    width: 100,
+    height: 100,
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+  },
+  text: {
+    fontWeight: 'bold',
   },
 });
--- src/views/ProductListing.tsx
+++ src/views/ProductListing.tsx
@@ -2,6 +2,7 @@ import {View, Text, StyleSheet} from 'react-native';
 import React, {useCallback, useState} from 'react';
 import Layout from '../layouts/Layout';
 import SearchHeader from '../widgets/SearchHeader';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 export const ProductListing = () => {
   const [search, setsearch] = useState<string>('');
@@ -14,7 +15,7 @@ export const ProductListing = () => {
     <Layout
       header={
         <SearchHeader
-          showBackButton={false}
+          showBackButton
           onEdit={e => {
             setsearch(e);
           }}
@@ -24,7 +25,30 @@ export const ProductListing = () => {
         />
       }>
       <View style={styles.container}>
-        <Text>Product</Text>
+        <View style={styles.item}>
+          <Icon name="cart" size={50} color="#000" />
+          <View style={styles.itemDetails}>
+            <Text style={styles.itemName}>Apple Watch</Text>
+            <Text style={styles.itemDesc}>This is a description</Text>
+          </View>
+          <Text style={styles.pricetext}>$ 13.00</Text>
+        </View>
+        <View style={styles.item}>
+          <Icon name="cart" size={50} color="#000" />
+          <View style={styles.itemDetails}>
+            <Text style={styles.itemName}>Apple Watch</Text>
+            <Text style={styles.itemDesc}>This is a description</Text>
+          </View>
+          <Text style={styles.pricetext}>$ 13.00</Text>
+        </View>
+        <View style={styles.item}>
+          <Icon name="cart" size={50} color="#000" />
+          <View style={styles.itemDetails}>
+            <Text style={styles.itemName}>Apple Watch</Text>
+            <Text style={styles.itemDesc}>This is a description</Text>
+          </View>
+          <Text style={styles.pricetext}>$ 13.00</Text>
+        </View>
       </View>
     </Layout>
   );
@@ -34,5 +58,33 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#ffffff',
+    padding: 15,
+    alignItems: 'center',
+    flexDirection: 'column',
+    gap: 10,
+  },
+  item: {
+    width: '100%',
+    padding: 15,
+    minHeight: 70,
+    borderBottomWidth: 1,
+    borderColor: '#e0e0e0',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: '10',
+  },
+  itemDetails: {
+    flex: 1,
+    height: '100%',
+  },
+  pricetext: {
+    fontWeight: 'bold',
+  },
+  itemName: {
+    fontWeight: 'bold',
+  },
+  itemDesc: {
+    fontSize: 14,
   },
 });
